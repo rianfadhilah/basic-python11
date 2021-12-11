23ddf44547fdf67c44940ab7d88a655beda66f9d
unknown <rianfadhillah17@gmail.com>
2021-12-11 15:24:28 +0700

    pertemuan 4

--- forloops.py
+++ forloops.py
@@ -23,7 +23,31 @@
 #        break
 #    print(i)
 x = 1
-for i in range(5):
-    for x in range (7):
-        x+=1
-        print(x)
+y=1
+#while y<=3:
+ #   y+=1
+ #   for x in range (7):
+  #      x+=1
+ #       print(x)
+
+#for i in range (11, -11, -1):
+#     for j in range(i):
+#         print(i, end = ' ')
+#     print()
+#x=[1,2,3,4,5]
+#y=[2,4,3,5,6]
+#z=0
+
+#for i in x:
+#    for j in y:
+#        if i==j:
+#            z=z+1
+#print(z)
+
+name=["ismali", "ali","elif"]
+number = [1,2,3]
+cars =["brio", "mobilio", "ayla"]
+for names in name:
+    for num in number:
+        for car in cars:
+            print(f"{names} mobilnya {cars}")
--- list.py
+++ list.py
@@ -2,5 +2,3 @@ daftar_belanja =["keset","lap","sapu"]
 daftar_belanja.insert(1,"sikat gigi")
 daftar_belanja.append("lap")
 #print(daftar_belanja[0][0])
-if "keset" in daftar_belanja:
-    print("ada keset")
